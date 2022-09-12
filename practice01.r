21515ca331f4ccfe3455f896f9198377b5fa987f
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-09-11 23:49:19 -0300

    feat: update files

---
+++ practice01.r
@@ -0,0 +1,12 @@
+# State
+s <- c("Alabama", "Alaska", "Arizona", "Arkansas", "Califormia", "Colorado", "Connecticut", "Delaware")
+# Population
+po <- c(4779736, 710231, 6392017, 2915918, 37253956, 5029796, 3574097, 897934)
+# Murder Rate
+mr <- c(5.7, 5.6, 4.7, 5.6, 4.4, 2.8, 2.4, 5.8)
+# Abbreviation
+a <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE")
+# Data Frame
+df <- data.frame(s, po, mr, a)
+
+df
